2c995c0220c764e65c675694157557621289df44
Rui Franco <rui3manuel@hotmail.com>
2017-04-19 22:30:35 +0100

    Adcionado ligação ao API de nutrição

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import { StackNavigator } from 'react-navigation';
 import ExampleComponent from './SearchBar';
 import ListViewDemo from './src/View/ListViewDemo';
 
-
 class HomeScreen extends React.Component {
   static navigationOptions = {
    title: 'Welcome',
@@ -19,6 +18,24 @@ class HomeScreen extends React.Component {
         titleStyle: { color: '#fafafa'},
     }
   };
+
+   gethealthLabelsFromApi() {
+     console.log("Ola")
+    return fetch('https://api.edamam.com/api/nutrition-data?app_id=f5025c6a&app_key=a2d609f71f42df915ed8c9d982028c38&ingr=1 large apple')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log (responseJson.totalNutrients);
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentDidMount() {
+    this.gethealthLabelsFromApi();
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -70,3 +87,7 @@ const styles = StyleSheet.create({
  
 
 AppRegistry.registerComponent('ProjetoBiologia', () => SimpleApp);
+
+//https://api.edamam.com/api/nutrition-data?app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&ingr=1%20large%20apple
+//APP_ID = f5025c6a
+//APP_KEY = a2d609f71f42df915ed8c9d982028c38
--- src/View/ListViewDemo.js
+++ src/View/ListViewDemo.js
@@ -11,15 +11,53 @@ const styles = StyleSheet.create({
 });
 
 class ListViewDemo extends React.Component {
+
+    gethealthLabelsFromApi() {
+      console.log("Ola")
+      return fetch('https://api.edamam.com/api/nutrition-data?app_id=f5025c6a&app_key=a2d609f71f42df915ed8c9d982028c38&ingr=1 large apple')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log (responseJson.totalNutrients);
+          var array = [];
+          for(var key in responseJson.totalNutrients){
+            array.push(responseJson.totalNutrients[key].label);
+            
+          }
+          console.log(array)
+          return array
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+
   constructor(props) {
     super(props);
 
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    const rows = ['row 1', 'row 2'];
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});0
     
     this.state = {
-      dataSource: ds.cloneWithRows(rows),
+      dataSource: this.ds.cloneWithRows([]),
     };
+  }
+
+  _completion(rows) {
+    console.log("_completion");
+    console.log("rows:");
+    console.log(rows);
+    console.log("old state:");
+    console.log( this.state);
+    this.setState({
+      dataSource: this.ds.cloneWithRows(rows)
+    });
+    console.log("new state:");
+    console.log(this.state);
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.gethealthLabelsFromApi()
+      .then((rows) => this._completion(rows));
   }
 
   render() {
